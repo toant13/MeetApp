d4d3e6e3ee60618220406a746e65e85545606bb3
Tony Tran <toant713@gmail.com>
2014-04-27 22:46:06 -0700

    Added test server with canned message

---
+++ GCMTestServer/src/main/java/com/example/server/GCMServerTester.java
@@ -0,0 +1,24 @@
+package com.example.server;
+
+import com.google.android.gcm.server.Message;
+import com.google.android.gcm.server.Result;
+import com.google.android.gcm.server.Sender;
+
+
+
+public class GCMServerTester {
+	  public static void main(String[] args) throws Exception {
+		String apiKey = "AIzaSyAuSmpawGorpIBi6Y1GxtnW2ZZvIrkJi_8";
+		String SENDER_ID = "381088048468";
+	    sendMessage(apiKey);
+	  }
+
+	  private static void sendMessage(String apiKey) throws Exception {
+	    String deviceId = "APA91bH4nWoYOYG1_tjssz_oIU1qTNI6uOhH9zXecqP70UdaDhXJE9pz8AnIPwj3DiuV3mZAAJIYjVPyiu5t29K7dB8T1ZvrGhb6PcfO9lucQurhNL-RFkugy_-A42S3fjLh22N6M3HFDX09552lPlOL4OhWuInPo7mBFNxsa0Wdlz8HazgvtW4";
+	    Sender sender = new Sender(apiKey);
+	    Message message = new Message.Builder().addData("data1", "hellow").build();
+	    
+	    Result result = sender.send(message, deviceId, 2);
+	    System.out.println("result: " + result);
+	  }
+}
